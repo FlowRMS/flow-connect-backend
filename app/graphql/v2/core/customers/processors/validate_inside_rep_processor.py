d908b15c647ef735857455f4a35b03be1f3c25c6
jamal <jsaied99@gmail.com>
2025-12-20 17:36:49 -0500

    Refactor ValidateInsideRepProcessor to include auth_info in constructor

--- app/graphql/v2/core/customers/processors/validate_inside_rep_processor.py
+++ app/graphql/v2/core/customers/processors/validate_inside_rep_processor.py
@@ -7,14 +7,15 @@ from app.core.processors import (
     entity_processor,
 )
 from app.graphql.v2.core.customers.models.customer import CustomerV2
-
+from commons.auth import AuthInfo
 
 @entity_processor(CustomerV2)
 class ValidateInsideRepProcessor(BaseProcessor[CustomerV2]):
     """Sets inside_rep_id to created_by_id if not provided."""
 
-    def __init__(self) -> None:
+    def __init__(self, auth_info: AuthInfo) -> None:
         super().__init__()
+        self.auth_info = auth_info
 
     @property
     def events(self) -> list[RepositoryEvent]:
@@ -22,7 +23,3 @@ class ValidateInsideRepProcessor(BaseProcessor[CustomerV2]):
 
     async def process(self, context: EntityContext[CustomerV2]) -> None:
         logger.info("Validating inside_rep_id for CustomerV2")
-        customer = context.entity
-
-        if not customer.inside_rep_id:
-            raise ValueError("inside_rep_id must be provided for CustomerV2")
