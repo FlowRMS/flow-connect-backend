cae464eb8e692ed561458ebff414f78e4859dec8
jamal <jsaied99@gmail.com>
2025-12-31 18:51:01 -0500

    feat: Update split_rate field to allow None values in OrderInsideRepResponse and QuoteInsideRepResponse

--- app/graphql/orders/strawberry/order_inside_rep_response.py
+++ app/graphql/orders/strawberry/order_inside_rep_response.py
@@ -12,7 +12,7 @@ from app.core.db.adapters.dto import DTOMixin
 class OrderInsideRepResponse(DTOMixin[OrderInsideRep]):
     id: UUID
     user_id: UUID
-    split_rate: Decimal
+    split_rate: Decimal | None
     position: int
 
     @classmethod
