bb993bff31655df99d8d66c659cd5f24c7ebc01c
Jorge <jorge@flowrms.com>
2026-01-28 12:32:01 -0600

    fix: register ResendNotificationService in provider discovery
    Add app.workers module to service provider discovery to fix
    "Providers for type ResendNotificationService not found" error.

--- app/graphql/service_providers.py
+++ app/graphql/service_providers.py
@@ -1,8 +1,9 @@
 import app.graphql
 import app.integrations
+import app.workers
 from app.graphql.provider_discovery import discover_providers
 
 service_providers = discover_providers(
-    modules=[app.graphql, app.integrations],
+    modules=[app.graphql, app.integrations, app.workers],
     class_suffix="service",
 )
